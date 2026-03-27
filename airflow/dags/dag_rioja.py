bde8bf82f871155d222398df76fb81be54486cd9
migueljulve <19839496+migueljulve@users.noreply.github.com>
2026-03-27 17:42:32 +0000

    airflow running 3 tasks

--- airflow/dags/dag_rioja.py
+++ airflow/dags/dag_rioja.py
@@ -1,6 +1,7 @@
 import sys
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from datetime import datetime, timedelta
 
 # Use the absolute path inside the container
@@ -25,7 +26,34 @@ with DAG(
     tags=['rioja', 'dlt']
 ) as dag:
 
+    # task 1 : run the dlt ingestion script
     task_ingest = PythonOperator(
         task_id='run_dlt_ingestion_script',
         python_callable=load_data
     )
+
+    # TASK 2: Load Weather Stations into BigQuery
+    task_load_weather = GCSToBigQueryOperator(
+        task_id='load_weather_tables_to_bq',
+        bucket='rioja_wine_lake_raw',
+        # DLT Path: dataset_name/resource_name/*.parquet
+        source_objects=['rioja_raw_data/weather_stations_raw/*.parquet'],
+        destination_project_dataset_table='project-1314e5d0-dc4a-4a2f-804.rioja_wine_data.weather_stations_raw',
+        source_format='PARQUET',
+        write_disposition='WRITE_TRUNCATE', # Overwrites to match dlt "replace"
+        autodetect=True,
+    )
+
+    # TASK 3: Load History Metadata into BigQuery
+    task_load_history = GCSToBigQueryOperator(
+        task_id='load_history_table_to_bq',
+        bucket='rioja_wine_lake_raw',
+        source_objects=['rioja_raw_data/rioja_wine_history/*.parquet'],
+        destination_project_dataset_table='project-1314e5d0-dc4a-4a2f-804.rioja_wine_data.rioja_wine_history',
+        source_format='PARQUET',
+        write_disposition='WRITE_TRUNCATE',
+        autodetect=True,
+    )
+
+    # --- Flow Logic ---
+    task_ingest >> [task_load_weather, task_load_history]
